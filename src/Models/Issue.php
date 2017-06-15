9ea373709f91897aec5775fc69f60a98b8b936bc
Attila Fulop <fulopattila122@gmail.com>
2017-06-15 16:56:30 +0300

    Added relations to issue model

--- src/Models/Issue.php
+++ src/Models/Issue.php
@@ -15,9 +15,15 @@ namespace Konekt\Stift\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Konekt\Stift\Contracts\Issue as IssueContract;
+use Konekt\User\Models\UserProxy;
 
+/**
+ * @property \Konekt\Stift\Contracts\Project $project
+ */
 class Issue extends Model implements IssueContract
 {
+    protected $dates = ['created_at', 'updated_at', 'due_on'];
+
     protected $fillable = [
         'id',
         'project_id',
@@ -33,4 +39,53 @@ class Issue extends Model implements IssueContract
         'assigned_to'
     ];
 
+    /**
+     * Relation for the associated project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function project()
+    {
+        return $this->hasOne(ProjectProxy::modelClass(), 'id', 'project_id');
+    }
+
+    /**
+     * Relation for the associated issueType
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function issueType()
+    {
+        return $this->hasOne(IssueTypeProxy::modelClass(), 'id', 'issue_type_id');
+    }
+
+    /**
+     * Relation for the associated severity
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function severity()
+    {
+        return $this->hasOne(SeverityProxy::modelClass(), 'id', 'severity_id');
+    }
+
+    /**
+     * Relation for the user created the issue
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function createdBy()
+    {
+        return $this->hasOne(UserProxy::modelClass(), 'id', 'created_by');
+    }
+
+    /**
+     * Relation for the user the issue is assigned to
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function assignedTo()
+    {
+        return $this->hasOne(UserProxy::modelClass(), 'id', 'assigned_to');
+    }
 }
--- tests/Feature/IssueTest.php
+++ tests/Feature/IssueTest.php
@@ -13,6 +13,7 @@
 namespace Konekt\Stift\Tests\Feature;
 
 
+use Carbon\Carbon;
 use Konekt\AppShell\Models\User;
 use Konekt\User\Models\UserProxy;
 use Konekt\Stift\Models\IssueProxy;
@@ -98,6 +99,90 @@ class IssueTest extends TestCase
         ]);
 
         $this->assertGreaterThan(0, $criticalBug->id);
+    }
+
+    public function testIssueHoldsAssociatedEntities()
+    {
+        $mediumTicket = IssueProxy::create([
+            'project_id'    => $this->project1->id,
+            'issue_type_id' => $this->ticket->id,
+            'severity_id'   => $this->medium->id,
+            'subject'       => 'We need some more money',
+            'description'   => 'Money is never enough. Could you please send us even more?',
+            'status'        => '',
+            'created_by'    => $this->user1->id,
+            'assigned_to'   => $this->user2->id
+        ]);
+
+        $this->assertEquals($this->project1->id, $mediumTicket->project->id,
+            'Issue\'s associated project should be obtained via the `project` property'
+        );
+
+        $this->assertEquals($this->ticket->id, $mediumTicket->issueType->id,
+            'Issue\'s associated type should be obtained via the `issueType` property'
+        );
+
+        $this->assertEquals($this->medium->id, $mediumTicket->severity->id,
+            'Issue\'s associated severity should be obtained via the `severity` property'
+        );
+
+        $this->assertEquals($this->user1->id, $mediumTicket->createdBy->id,
+            'Issue\'s associated user that created the issue should be obtained via the `createdBy` property'
+        );
+
+        $this->assertEquals($this->user2->id, $mediumTicket->assignedTo->id,
+            'Issue\'s assigned user should be available via the `assignedTo` property'
+        );
+    }
+
+    public function testIssueDueOnField()
+    {
+        $lowTask = IssueProxy::create([
+            'project_id'    => $this->project1->id,
+            'issue_type_id' => $this->task->id,
+            'severity_id'   => $this->low->id,
+            'subject'       => 'Buy some cranberries',
+            'status'        => '',
+            'created_by'    => $this->user1->id
+        ]);
+
+        $this->assertNull($lowTask->due_on, 'Issue without due date should return null');
+
+        $mediumTask = IssueProxy::create([
+            'project_id'    => $this->project2->id,
+            'issue_type_id' => $this->task->id,
+            'severity_id'   => $this->medium->id,
+            'subject'       => 'Get some birthday gifts for Louis',
+            'status'        => '',
+            'created_by'    => $this->user2->id,
+            'due_on'        => '2017-09-26 23:59:59'
+        ]);
+
+        $this->assertInstanceOf(Carbon::class, $mediumTask->due_on);
+        $this->assertTrue(
+            $mediumTask->due_on->eq(
+                Carbon::parse('2017-09-26 23:59:59')
+            ),
+            'Due date should be specified via string'
+        );
+
+        $highTask = IssueProxy::create([
+            'project_id'    => $this->project1->id,
+            'issue_type_id' => $this->task->id,
+            'severity_id'   => $this->high->id,
+            'subject'       => 'Go Shopping NOW!!',
+            'status'        => '',
+            'created_by'    => $this->user1->id,
+            'due_on'        => Carbon::parse('2017-06-11 12:00:00')
+        ])->fresh();
+
+        $this->assertInstanceOf(Carbon::class, $highTask->due_on);
+        $this->assertTrue(
+            $highTask->due_on->eq(
+                Carbon::parse('2017-06-11 12:00:00')
+            ),
+            'Due date should be set with Carbon object'
+        );
     }
 
     protected function createTestData()
